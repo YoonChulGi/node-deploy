9f2c9c464b22ca722119b6823e1524e3bf2fa0d5
YoonChulGi <dbscjfrl321@gmail.com>
2022-02-15 23:26:36 +0900

    저장안된거 커밋

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 const helmet = require('helmet');
 const hpp = require('hpp');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
 dotenv.config();
+const redisClient = redis.createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASSWORD,
+});
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
@@ -55,7 +61,8 @@ const sessionOption = {
         httpOnly: true,
         secure: false,
     },
-};
+    store: new RedisStore({ client: redisClient }),
+}; 
 if(process.env.NODE_ENV === 'production') {
     sessionOption.proxy = true;
     // sessionOption.cookie.secure = true;
